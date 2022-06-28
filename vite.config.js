0826dd63690f2c4bf2a0b5c7e6030ae1b6e48ac3
furkancanzirek <can.zirek@ogr.sakarya.edu.tr>
2022-06-28 16:10:56 +0300

    vite configjs update

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,12 @@ import { defineConfig } from "vite";
 import { fileURLToPath, URL } from "url";
 import vue from "@vitejs/plugin-vue";
 
-// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-import vuetify from 'vite-plugin-vuetify'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
 		vue(),
-		vuetify({ autoImport: true }),
     
 ],
 server: { 
