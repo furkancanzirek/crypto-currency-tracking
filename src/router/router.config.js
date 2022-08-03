c7a430c21fbdafa1f4b359dee7d843f862c99d69
furkancanzirek <can.zirek@ogr.sakarya.edu.tr>
2022-08-03 12:54:34 +0300

    Update router.config.js

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -6,30 +6,30 @@ const router = createRouter({
   routes,
 });
 
-router.beforeEach((to, from, next) => {
-  if (to.matched.some((record) => record.meta.requiresAuth)) {
-    if (getAuth.currentUser || localStorage.getItem("authKey")) {
-      console.log("burada 1");
-      next();
-    } else {
-      console.log("burada 2");
-      next("/");
-    }
-    console.log("burada 3");
-  } else {
-    console.log("burada 4");
-    if (getAuth.currentUser || localStorage.getItem("authKey")) {
-      console.log("burada 5");
-      if (to.path=="/") {
-        console.log("burada 6");
-        next("/dashboard");
-      }
-    }
-    else{
-      next();
-    }
+// router.beforeEach((to, from, next) => {
+//   if (to.matched.some((record) => record.meta.requiresAuth)) {
+//     if (getAuth.currentUser || localStorage.getItem("authKey")) {
+//       console.log("burada 1");
+//       next();
+//     } else {
+//       console.log("burada 2");
+//       next("/");
+//     }
+//     console.log("burada 3");
+//   } else {
+//     console.log("burada 4");
+//     if (getAuth.currentUser || localStorage.getItem("authKey")) {
+//       console.log("burada 5");
+//       if (to.path=="/") {
+//         console.log("burada 6");
+//         next("/dashboard");
+//       }
+//     }
+//     else{
+//       next();
+//     }
    
    
-  }
-});
+//   }
+// });
 export default router;
