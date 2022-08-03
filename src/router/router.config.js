718947b3823c37cf84a2efff7311e8ee3e0d4b36
furkancanzirek <can.zirek@ogr.sakarya.edu.tr>
2022-08-03 12:49:33 +0300

    Update router.config.js

--- src/router/router.config.js
+++ src/router/router.config.js
@@ -9,17 +9,23 @@ const router = createRouter({
 router.beforeEach((to, from, next) => {
   if (to.matched.some((record) => record.meta.requiresAuth)) {
     if (getAuth.currentUser || localStorage.getItem("authKey")) {
+      console.log("burada 1");
       next();
     } else {
+      console.log("burada 2");
       next("/");
     }
+    console.log("burada 3");
   } else {
+    console.log("burada 4");
     if (getAuth.currentUser || localStorage.getItem("authKey")) {
+      console.log("burada 5");
       if (to.path=="/") {
+        console.log("burada 6");
         next("/dashboard");
       }
     }
-
+    console.log("burada 7");
     next();
   }
 });
